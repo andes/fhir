9c9f2542e4f4c55ee7fc79270f77f90989e6d900
sychus <hfernandez@neuquen.gov.ar>
2021-04-15 02:01:00 -0300

    test and actions

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    roots: ['./'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testEnvironment: 'node',
+    moduleNameMapper: {
+        '@andes/unit-test': '<rootDir>/packages/unit-testing/index.ts'
+    }
+}
