98aa8d08c0e5f62b712583726254c8ece61b603f
ponjaBob <jiramirez_nqn@hotmail.com>
2025-11-07 14:54:36 -0300

    feat: devolver todos los identifier

--- src/lib/patient.test.ts
+++ src/lib/patient.test.ts
@@ -2,39 +2,43 @@ import * as patient from '../data/patient.json';
 import { decode, encode } from './patient';
 import { createPatient } from '../helpers/patientFactory';
 
+const pacienteFhir = encode(patient);
+
 describe('encode patient from ANDES to FHIR R4', () => {
     test('Verify basic data', () => {
-        expect(encode(patient).resourceType).toBe('Patient');
-        expect(encode(patient).identifier[0].value).toBe('42910660');
-        expect(encode(patient).identifier[0].system).toBe('http://www.renaper.gob.ar/dni');
-        expect(encode(patient).active).toBe(true);
-        expect(encode(patient).name[0].use).toBe('official');
-        expect(encode(patient).name[0].resourceType).toBe('HumanName');
-        expect(encode(patient).name[0].family).toContain('PERINGA');
-        expect(encode(patient).name[0].family).toContain('PONCE');
-        expect(encode(patient).name[0].given).toContain('AGUSTIN');
-        expect(encode(patient).name[0].text).toBe('HUGO AGUSTIN PERINGA PONCE');
-        expect(encode(patient).gender).toBe('male');
-        expect(encode(patient).birthDate).toBe('1979-11-14');
+        expect(pacienteFhir.resourceType).toBe('Patient');
+        const ident = pacienteFhir.identifier;
+        const id = ident?.find(t => t.system === 'http://www.renaper.gob.ar/dni');
+        expect(id?.value).toBe('42910660');
+        expect(id?.system).toBe('http://www.renaper.gob.ar/dni');
+        expect(pacienteFhir.active).toBe(true);
+        expect(pacienteFhir.name[0].use).toBe('official');
+        expect(pacienteFhir.name[0].resourceType).toBe('HumanName');
+        expect(pacienteFhir.name[0].family).toContain('PERINGA');
+        expect(pacienteFhir.name[0].family).toContain('PONCE');
+        expect(pacienteFhir.name[0].given).toContain('AGUSTIN');
+        expect(pacienteFhir.name[0].text).toBe('HUGO AGUSTIN PERINGA PONCE');
+        expect(pacienteFhir.gender).toBe('male');
+        expect(pacienteFhir.birthDate).toBe('1979-11-14');
     });
     test('Verify patient with contact information', () => {
         //  Phone;
-        expect(encode(patient).telecom[0].resourceType).toBe('ContactPoint');
-        expect(encode(patient).telecom[0].value).toBe('4462221');
-        expect(encode(patient).telecom[0].system).toBe('phone');
+        expect(pacienteFhir.telecom[0].resourceType).toBe('ContactPoint');
+        expect(pacienteFhir.telecom[0].value).toBe('4462221');
+        expect(pacienteFhir.telecom[0].system).toBe('phone');
         //   Address;
-        expect(encode(patient).address[0].resourceType).toBe('Address');
-        expect(encode(patient).address[0].postalCode).toBe('8300');
-        expect(encode(patient).address[0].line).toContain('Enrique Santos Discepolo 1856');
-        expect(encode(patient).address[0].city).toBe('NEUQUEN');
-        expect(encode(patient).address[0].state).toBe('Neuquén');
-        expect(encode(patient).address[0].country).toBe('Argentina');
+        expect(pacienteFhir.address[0].resourceType).toBe('Address');
+        expect(pacienteFhir.address[0].postalCode).toBe('8300');
+        expect(pacienteFhir.address[0].line).toContain('Enrique Santos Discepolo 1856');
+        expect(pacienteFhir.address[0].city).toBe('NEUQUEN');
+        expect(pacienteFhir.address[0].state).toBe('Neuquén');
+        expect(pacienteFhir.address[0].country).toBe('Argentina');
     });
     test('Verify patient with relationships', () => {
-        expect(encode(patient).contact[0].relationship[0].text).toBe('hijo/a');
-        expect(encode(patient).contact[0].name.resourceType).toBe('HumanName');
-        expect(encode(patient).contact[0].name.family).toContain('PERINGA');
-        expect(encode(patient).contact[0].name.given).toContain('JOSEFINA');
+        expect(pacienteFhir.contact[0].relationship[0].text).toBe('hijo/a');
+        expect(pacienteFhir.contact[0].name.resourceType).toBe('HumanName');
+        expect(pacienteFhir.contact[0].name.family).toContain('PERINGA');
+        expect(pacienteFhir.contact[0].name.given).toContain('JOSEFINA');
     });
 
     test('Maneja paciente con estado validado', () => {
@@ -56,7 +60,42 @@ describe('encode patient from ANDES to FHIR R4', () => {
     });
 
     it('Verifica organización que crea paciente', () => {
-        const fhir = encode(patient);
+        const fhir = pacienteFhir;
         expect(fhir.managingOrganization.display).toContain('LABORATORIO');
+    });
+
+    it('Verifica dni extranjero', () => {
+        const paciente = createPatient({ tipoIdentificacion: 'dni extranjero', numeroIdentificacion: 'ABC123' });
+        const fhir = encode(paciente);
+        const id = fhir.identifier?.find(t => t.system === 'andes.gob.ar/sid/foreign-id');
+        expect(id?.value).toBe('ABC123');
+        expect(id?.system).toBe('andes.gob.ar/sid/foreign-id');
+    });
+
+    it('Verifica pasaporte', () => {
+        const paciente = createPatient({ tipoIdentificacion: 'pasaporte', numeroIdentificacion: 'ABC123CD' });
+        const fhir = encode(paciente);
+        const id = fhir.identifier?.find(t => t.system === 'andes.gob.ar/sid/passport');
+        expect(id?.value).toBe('ABC123CD');
+        expect(id?.system).toBe('andes.gob.ar/sid/passport');
+    });
+});
+
+describe('decode patient from FHIR R4 to ANDES', () => {
+    it('Verifica dni extranjero', () => {
+        const paciente = createPatient({ tipoIdentificacion: 'dni extranjero', numeroIdentificacion: 'ABC123' });
+        const fhirEncode = encode(paciente);
+        const fhirDecode = decode(fhirEncode);
+        expect(fhirDecode?.tipoIdentificacion).toBe('dni extranjero');
+        expect(fhirDecode?.numeroIdentificacion).toBe('ABC123');
+    });
+
+    it('Verifica pasaporte', () => {
+        const paciente = createPatient({ tipoIdentificacion: 'pasaporte', numeroIdentificacion: 'ABC123CD' });
+        const fhirEncode = encode(paciente);
+        const fhirDecode = decode(fhirEncode);
+        expect(fhirDecode?.tipoIdentificacion).toBe('pasaporte');
+        expect(fhirDecode?.numeroIdentificacion).toBe('ABC123CD');
+        expect(fhirDecode?.estado).toBe('temporal');
     });
 });
