dcb7051e39acb742ee9fe4678784aca2710131ca
jrojas <cesarlinux@gmail.com>
2026-09-07 13:21:32 -0300

    chore: cambio FHIR identifier system constants

--- src/constants/identifier-systems.ts
+++ src/constants/identifier-systems.ts
@@ -1,8 +1,8 @@
 export const FhirIdentifierSystems = {
     ANDES_ID: 'http://andes.gob.ar',
     MATRICULACIONES: 'http://andes.gob.ar/matriculaciones',
-    DNI: 'http://www.renaper.gob.ar/dni',
-    CUIL: 'http://www.renaper.gob.ar/cuil',
+    DNI: 'https://www.renaper.gob.ar/dni',
+    CUIL: 'https://www.anses.org.ar/cuil',
     PASSPORT: 'http://andes.gob.ar/sid/passport',
     FOREIGN_ID: 'http://andes.gob.ar/sid/foreign-id',
     PRESCRIPTIONS: 'http://app.andes.gob.ar/prescriptions',
